3d6222155903d7901cf6cf47c0fbab1ec3bcc8c7
Sophie Willis <s.willis@lounge.com>
2023-04-05 17:00:01 +0100

    feat: airbnb clone

---
+++ app/components/Listings/ListingReservation.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Range } from "react-date-range";
+
+import Calendar from "../Inputs/Calendar";
+import Button from "../Button";
+
+interface ListingReservationProps {
+  price: number;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  dateRange: Range;
+  onSubmit: () => void;
+  disabled: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  onChangeDate,
+  dateRange,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div className="bg-white rounded-xl border border-neutral-200 overflow-hidden">
+      <div className="flex items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="font-light text-neutral-600">per night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+      <hr />
+      <div className="p-4">
+        <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
+      </div>
+      <div className="p-4 flex items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ {totalPrice}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingReservation;
---
+++ app/components/Modals/RegisterModal.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import Heading from "../Heading";
+import Input from "../Inputs/Input";
+import Button from "../Button";
+import Modal from "./Modal";
+import { signIn } from "next-auth/react";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Success!");
+        registerModal.onClose();
+        loginModal.onOpen();
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsLoading(false));
+  };
+
+  const toggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create and account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn("google")}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn("github")}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex items-center gap-2">
+          <div>Already have an account?</div>
+          <div
+            onClick={toggle}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default RegisterModal;
